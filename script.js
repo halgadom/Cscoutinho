08f428d635bd7f304e14211edc84d14db0662e1b
halgadom <79438760+halgadom@users.noreply.github.com>
2023-05-09 21:06:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("btn-convert").addEventListener("click", function() {
     const amount = document.getElementById("amount").value;
     const conversionRate = 4.99;
-    const result = amount * conversionRate;
+    const result = amount / conversionRate;
     document.getElementById("result").setAttribute("style", "display:block");
     document.getElementById("txt-primary").innerText = amount + " Real = ";
     document.getElementById("txt-secondary").innerText = result.toFixed(2) + " Dollar";
